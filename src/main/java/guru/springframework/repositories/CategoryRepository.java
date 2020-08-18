637990cbd57cf6821fb749d8cf81096504c26fd2
Annasarp-blr <pras_sunny@yahoo.com>
2020-08-18 10:54:17 +0530

    dwdsdssds

--- src/main/java/guru/springframework/repositories/CategoryRepository.java
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -1,8 +1,9 @@
 package guru.springframework.repositories;
 
+import guru.springframework.domain.Categories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<CategoryRepository, Long> {
+public interface CategoryRepository extends CrudRepository<Categories, Long> {
 }
